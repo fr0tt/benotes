e0014c8f465536f8d2bdc1570dac1662cfcde80b
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-08-07 01:07:50 +0200

    add additional packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +13,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
