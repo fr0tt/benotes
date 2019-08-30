087e9c39973a19cec2c93503a2962fd90f614bca
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-08-30 02:00:52 +0200

    relocate routes in their own file & install vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+import {routes} from './routes.js'
 
 Vue.use(VueRouter)
 
-import Example from './components/ExampleComponent'
-
 const router = new VueRouter({
     mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'example',
-            component: Example,
-            props: { title: "This is the SPA home" }
-        },
-    ],
+    routes
 })
+
 const app = new Vue({
     el: '#app',
     router
-});
+})
---
+++ resources/js/routes.js
@@ -0,0 +1,9 @@
+import Example from './components/ExampleComponent'
+
+export const routes = [
+    {
+        path: '/',
+        name: 'example',
+        component: Example
+    },
+]
