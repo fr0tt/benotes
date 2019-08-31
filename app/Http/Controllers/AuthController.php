ec79ea72e5a482c3fc596e9c84c78ba01c5d7467
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-09-01 01:44:27 +0200

    correct minor flaws

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
 
     public function me(Request $request)
     {
-        return response()->json($request->user());
+        return response()->json(['data' => $request->user()]);
     }
 
     public function refresh()
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+const mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss')
 
 /*
  |--------------------------------------------------------------------------
@@ -16,5 +16,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
-    });
+        postCss: [tailwindcss('./tailwind.config.js')]
+    })
