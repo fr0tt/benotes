78040a8997bf49f188af24e3e7125a82181ffc6c
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-09-09 00:54:16 +0200

    some css ajustments and added text type

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,9 +1,9 @@
 <template>
-    <li v-if="post.type === 'link'" class="inline-block m-4">
-        <div class="w-card rounded overflow-hidden shadow-lg">
+    <li class="inline-block m-4">
+        <div v-if="post.type === 'link'" class="w-card h-card rounded overflow-hidden shadow-lg">
             <a :href="post.url" target="_blank">
-                <div v-if="post.image_path" class="cover w-full bg-cover bg-center" :style="image"></div>
-                <div v-else class="cover w-full flex items-center justify-center" :style="color">
+                <div v-if="post.image_path" class="h-cover w-full bg-cover bg-center" :style="image"></div>
+                <div v-else class="h-cover w-full flex items-center justify-center" :style="color">
                     <span class="text-white text-2xl font-medium">{{ domain }}</span>
                 </div>
             </a>
@@ -14,6 +14,11 @@
             <div class="px-6 py-4 truncate">
                 <img :src="'https://www.google.com/s2/u/0/favicons?domain=' + domain" class="inline img-vertical-align">
                 <a :href="post.url" target="_blank" class="text-blue-600">{{ post.url }}</a>
+            </div>
+        </div>
+        <div v-else class="w-card h-card rounded overflow-hidden shadow-lg bg-gray-100">
+            <div class="p-6">
+                <p class="text-gray-900 text-xl overflow-hidden">{{ post.content }}</p>
             </div>
         </div>
     </li>
@@ -39,8 +44,11 @@ export default {
     .w-card {
         width: 22rem;
     }
-    .cover {
-        height: 200px;
+    .h-card {
+        height: 22.5rem;
+    }
+    .h-cover {
+        height: 12.5rem;
     }
     .description {
         height: 3rem;
