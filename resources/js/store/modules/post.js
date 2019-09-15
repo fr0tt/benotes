65d14b466bdc9cce65fd1a2a157ce737efe43e49
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-09-15 10:46:33 +0200

    change how to get uncategorized posts & update posts

--- resources/js/store/modules/post.js
+++ resources/js/store/modules/post.js
@@ -13,17 +13,29 @@ export default {
         addPost (state, post) {
             state.posts.push(post)
         },
+        setPost (state, post, index) {
+            state.posts[index] = post
+        },
         setCurrentPost (state, post) {
             state.currentPost = post
+        },
+        setCurrentPostContent (state, content) {
+            state.currentPost.content = content
         }
     },
     actions: {
         fetchPosts (context, collectionId) {
-            axios.get('/api/posts', {
-                params: {
+            let params = null
+            if (typeof collectionId === 'undefined' || collectionId === null) {
+                params = {
+                    is_uncategorized: true
+                }
+            } else {
+                params = {
                     collection_id: collectionId
                 }
-            })
+            }
+            axios.get('/api/posts', params)
                 .then(response => {
                     const posts = response.data.data
                     context.commit('setPosts', posts)
@@ -37,6 +49,26 @@ export default {
         },
         setCurrentPost (context, post) {
             context.commit('setCurrentPost', post)
+        },
+        setCurrentPostContent (context, content) {
+            context.commit('setCurrentPostContent', content)
+        },
+        updatePost (context, currentPost) {
+            axios.patch('/api/posts/' + currentPost.id, {
+                content: currentPost.content
+            })
+                .then(response => {
+                    const newPost = response.data.data
+                    context.state.posts.find((post, i) => {
+                        if (post.id === newPost.id) {
+                            context.commit('setPost', post, i)
+                            return
+                        }
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
         }
     }
 }
