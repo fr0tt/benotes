3d67e754d6dc712ee522c8263f6fd25afbfda5c4
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-11-09 17:39:23 +0100

    design

--- resources/js/components/CreatePostComponent.vue
+++ resources/js/components/CreatePostComponent.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="fixed bottom-0 w-full left-0 bg-gray-200">
-        <form @submit.prevent="create" @keyup.ctrl.enter="create" class="flex mx-6 my-6">
-            <div class="w-1/6"></div>
-            <div class="w-4/6">
+        <form @submit.prevent="create" @keyup.ctrl.enter="create" class="flex md:mx-6 mx-1 md:my-6 mt-2">
+            <div class="lg:w-1/6"></div>
+            <div class="lg:w-4/6 flex-1">
                 <textarea ref="content" class="w-full rounded-full border-2 outline-none border-gray-400 
-                    bg-white py-2 px-5 text-lg leading-snug focus:border-blue-400"></textarea>
+                    bg-white py-2 px-5 text-lg leading-snug focus:border-blue-400" 
+                    :rows="isMobile() ? '1' : '2'"></textarea>
                 <!--<div class="">
                     <span> save in: </span> <input class="text-bold text-blue-600">Uncateogirzed</input>
                 </div>-->
             </div>
-            <div class="w-1/6">
-                <button class="border-2 border-blue-500 rounded-full py-2 px-3 text-blue-600 bg-white
-                    leading-tight text-xl font-semibold mx-4 my-1 px-6 hover:bg-blue-500 hover:text-white">
+            <div class="lg:w-1/6">
+                <button class="border-2 border-blue-500 rounded-full outline-none
+                    leading-tight md:text-xl font-semibold md:mx-4 ml-1 md:px-6 px-5 py-2.5
+                    bg-blue-500 text-white hover:text-blue-600 hover:bg-white">
                     Save
                 </button>
-                <p class="ml-6 text-sm text-gray-600">Strg + Enter</p>
+                <p class="ml-6 text-sm text-gray-600 hidden md:block">Strg + Enter</p>
             </div>
         </form>
     </div>
@@ -38,6 +40,9 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+        },
+        isMobile () {
+            return (screen.width <= 600) ? true : false
         }
     },
     computed: {
@@ -80,6 +85,11 @@ textarea {
 ::-webkit-scrollbar-thumb {
 	@apply bg-blue-600;
     border-radius: 8px;
+}
+
+.py-2\.5 {
+    padding-top: 0.625rem;
+    padding-bottom: 0.625rem;
 }
 
 </style>
