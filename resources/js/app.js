a8280e66dab58e56005b025ade47ad57a9ed45b3
fr0tt <33751346+fr0tt@users.noreply.github.com>
2019-12-29 10:34:46 +0100

    add vue svg

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueCookie from 'vue-cookie'
+import SvgVue from 'svg-vue'
 
 import routes from './routes.js'
 import store from './store'
 
 Vue.use(VueCookie)
 Vue.use(VueRouter)
+Vue.use(SvgVue)
 
 const router = new VueRouter({
     mode: 'history',
