469a31c9b093c225bcd9ea5cef03486a1e7c5b57
fr0tt <33751346+fr0tt@users.noreply.github.com>
2020-02-19 16:51:24 +0100

    change orange-600

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    theme: {
+        extend: {
+            colors: {
+                orange: {
+                    ...colors.orange,
+                    '600': '#FF7700'
+                }
+            }
+        }
+    },
+    variants: {},
+    plugins: []
 }
