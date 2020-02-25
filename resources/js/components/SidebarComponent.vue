6bfcbe8ac1db5ff38449fe8ed12f39c450e26601
fr0tt <33751346+fr0tt@users.noreply.github.com>
2020-02-25 01:09:17 +0100

    add menu

--- resources/js/components/SidebarComponent.vue
+++ resources/js/components/SidebarComponent.vue
@@ -3,13 +3,30 @@
         <div v-if="isOpen" class="sidebar pb-6 box-shadow mr-4 md:w-48 xl:w-1/6">
             <div class="flex px-8 py-4">
                 <div class="relative flex-1">
-                    <router-link to="/users/me">
-                        <svg class="w-5 mr-2 align-text-bottom fill-current text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
-                        <span class="text-lg">{{ authUser.name }}</span>
-                    </router-link>
-                    <svg-vue class="w-2 ml-2 text-gray-600 cursor-pointer fill-current inline-block" icon="arrow_down"/>
+                    <div class="absolute mt--4 min-w-48 bg-white" :class="{ 'rounded shadow': menuIsOpen }">
+                        <div @click="menuIsOpen = !menuIsOpen" class="p-4 cursor-pointer">
+                            <svg class="w-5 mr-2 align-text-bottom fill-current text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
+                            <span class="text-lg text-gray-700">{{ authUser.name }}</span>
+                            <svg-vue class="w-2.5 ml-2 text-gray-700 cursor-pointer fill-current inline-block" icon="arrow_down"/>
+                        </div>
+                        <transition name="fade">
+                            <div v-if="menuIsOpen" @click="menuIsOpen = false" class="pb-1">
+                                <hr class="m-0 mb-1 border-t border-gray-200">
+                                <!-- <router-link class="block py-1 px-4 font-medium text-gray-700">
+                                    Settings
+                                </router-link> -->
+                                <router-link to="/users/me" class="menuItem">
+                                    My Account
+                                </router-link>
+                                <!-- <hr class="m-0 mt-1 border-t border-gray-200"> -->
+                                <a href="/" @click="logout()" class="menuItem py-2">
+                                    Logout
+                                </a>
+                            </div>
+                        </transition>
+                    </div>
                 </div>
-                <svg @click="toggle()" class="menu-icon text-gray-500 cursor-pointer fill-current inline-block"
+                <svg @click="toggle()" class="menu-icon text-gray-600 cursor-pointer fill-current inline-block"
                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z"></path>
                 </svg>
             </div>
@@ -50,11 +67,13 @@
 
 <script>
 import { mapState } from 'vuex'
+import axios from 'axios'
 export default {
     name: 'Sidebar',
     data () {
         return {
-            isOpen: (localStorage.getItem('sidebar') === 'false') ? false : true
+            isOpen: (localStorage.getItem('sidebar') === 'false') ? false : true,
+            menuIsOpen: false
         }
     },
     methods: {
@@ -64,6 +83,12 @@ export default {
         toggle () {
             localStorage.setItem('sidebar', !this.isOpen)
             this.isOpen = !this.isOpen
+        },
+        logout () {
+            axios.post('/api/auth/logout')
+                .catch(error => {
+                    console.log(error.response)
+                })
         }
     },
     computed: {
@@ -81,6 +106,19 @@ export default {
 </script>
 
 <style lang="scss">
+    .mt--4 {
+        margin-top: -1rem;
+    }
+    .w-2\.5 {
+        width: 0.625rem;
+    }
+    .min-w-48 {
+        min-width: 12rem;
+    }
+    .py-1\.5 {
+        padding-top: 0.375rem;
+        padding-bottom: 0.375rem;
+    }
     .sidebar {
         box-shadow: 2px 3px 3px 0 rgba(0, 0, 0, 0.1);
         transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s;
@@ -92,11 +130,24 @@ export default {
         .slide-enter, .slide-leave-to {
             width: 0;
         }
+        .fade-enter-active, .fade-leave-active {
+            transition: opacity .2s;
+        }
+        .fade-enter, .fade-leave-to {
+            opacity: 0;
+        }
         svg {
             display: inline-block;
         }
         .menu-icon {
             width: 1.75rem;
+        }
+        .menuItem {
+            @apply block py-1.5 px-4 font-medium text-gray-700;
+            transition: 0.2s background-color;
+        }
+        .menuItem:hover {
+            @apply bg-gray-300;
         }
         .list {
             .collection {
