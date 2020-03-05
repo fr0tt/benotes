6aaf62336d65addfdfa1be700acc64dd4a82dffc
fr0tt <33751346+fr0tt@users.noreply.github.com>
2020-03-05 13:52:23 +0100

    add permission

--- app/User.php
+++ app/User.php
@@ -13,6 +13,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 {
     use Authenticatable, Authorizable;
 
+    const ADMIN = 255;
+
     /**
      * The attributes that are mass assignable.
      *
