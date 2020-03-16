b4e3996acbf0b48eb67ab4c0e1fe2fbfe5fa3944
fr0tt <33751346+fr0tt@users.noreply.github.com>
2020-03-16 14:43:03 +0100

    clean up

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-
-use App\User;
 
 class AuthController extends Controller
 {
