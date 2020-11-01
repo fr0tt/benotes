82ec0de55071277a12d326623fa1aebc25bc375f
fr0tt <33751346+fr0tt@users.noreply.github.com>
2020-11-01 19:52:08 +0100

    liberate appbar

---
+++ resources/js/components/Appbar.vue
@@ -0,0 +1,117 @@
+<template>
+    <div>
+        <div class="flex sm:px-8 px-4 py-3">
+            <div class="w-16 my-auto">
+                <button @click="toggleSidebar()" class="align-bottom">
+                    <svg-vue class="w-6 cursor-pointer" icon="remix/menu-line"/>
+                </button>
+            </div>
+            <div class="flex-1 my-auto text-center">
+                <span class="text-gray-800 font-medium text-xl">{{ currentCollection.name }}</span>
+                <div class="relative inline-block" v-if="currentCollection.id > 0">
+                    <button @click="showContextMenu = !showContextMenu" class="align-text-bottom">
+                        <svg-vue class="w-5 mb-1/5" icon="remix/more-2-fill"/>
+                    </button>
+                    <transition name="fade">
+                        <ol v-if="showContextMenu" class="absolute bg-white shadow-lg contextmenu z-50">
+                            <li @click="edit()">
+                                Edit
+                                <svg class="context-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+                            </li>
+                            <li @click="del()">
+                                Delete
+                                <svg class="context-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
+                            </li>
+                        </ol>
+                    </transition>
+                </div>
+            </div>
+            <div v-if="permission >= 6" class="">
+                <button v-if="isSupported" class="button" @click="pasteNewPost()">
+                    <svg-vue class="button-icon" icon="zondicons/paste"/>
+                    Paste
+                </button>
+                <router-link :to="`/c/${currentCollection.id}/p/create`" class="button ml-4"
+                    tag="button" title="Strg + Alt + N">
+                    <svg-vue class="button-icon" icon="zondicons/add-outline"/>
+                    Create
+                </router-link>
+            </div>
+        </div>
+        <hr class="block border-t-2 border-orange-600">
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Sidebar from './Sidebar.vue'
+export default {
+    name: 'Appbar',
+    data () {
+        return {
+            isOpen: true,
+            showContextMenu: false
+        }
+    },
+    components: {
+        Sidebar
+    },
+    methods: {
+        pasteNewPost () {
+            // may only work with a secure connection
+            navigator.clipboard.readText().then(content => {
+                if (content === '' || content === null) {
+                    return
+                }
+                axios.post('/api/posts', {
+                    content: content,
+                    collection_id: this.currentCollection.id
+                })
+                    .then(response => {
+                        this.$store.dispatch('post/addPost', response.data.data)
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            })
+        },
+        edit () {
+            this.showContextMenu = false
+            this.$router.push({ path: '/c/' + this.currentCollection.id + '/edit' })
+        },
+        del () {
+            this.showContextMenu = false
+            this.$store.dispatch('collection/deleteCollection', this.currentCollection.id)
+            this.$router.push({ path: '/' })
+        },
+        toggleSidebar () {
+            console.log('sdfsf')
+            this.$store.dispatch('toggleSidebar')
+        }
+    },
+    computed: {
+        isSupported () {
+            if (typeof navigator.clipboard === 'undefined') {
+                return false
+            }
+            if (typeof navigator.clipboard.readText === 'undefined') {
+                return false
+            }
+            if (navigator.clipboard.readText() !== null) {
+                return true
+            }
+            return false
+        },
+        ...mapState('collection', [
+            'currentCollection'
+        ]),
+        ...mapState('auth', [
+            'permission'
+        ])
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>
--- resources/js/components/Collection.vue
+++ resources/js/components/Collection.vue
@@ -1,43 +1,10 @@
 <template>
     <div>
-        <div class="md:flex sm:px-8 px-2 py-3 shadow-md">
-            <div class="flex-1 md:mb-0 mb-2">
-                <span class="text-orange-600 font-semibold text-2xl font-sans">{{ currentCollection.name }}</span>
-                <div class="relative inline-block" v-if="currentCollection.id > 0">
-                    <button @click="showContextMenu = !showContextMenu" class="align-text-bottom">
-                        <svg-vue class="w-5 mb-1/5" icon="material/more_vert"/>
-                    </button>
-                    <transition name="fade">
-                        <ol v-if="showContextMenu" class="absolute bg-white shadow-lg contextmenu z-50">
-                            <li @click="edit()">
-                                Edit
-                                <svg class="context-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
-                            </li>
-                            <li @click="del()">
-                                Delete
-                                <svg class="context-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
-                            </li>
-                        </ol>
-                    </transition>
-                </div>
-            </div>
-            <div v-if="permission >= 6" class="">
-                <button v-if="isSupported" class="button" @click="pasteNewPost()">
-                    <svg-vue class="button-icon" icon="zondicons/paste"/>
-                    Paste
-                </button>
-                <router-link :to="`/c/${this.id}/p/create`" class="button ml-4"
-                    tag="button" title="Strg + Alt + N">
-                    <svg-vue class="button-icon" icon="zondicons/add-outline"/>
-                    Create
-                </router-link>
-            </div>
-        </div>
-        <div class="sm:ml-4 -ml-2">
+        <div class="sm:ml-4 -ml-2 px-2">
             <transition name="collection-fade">
                 <Draggable v-if="!isLoading" tag="ol" v-model="posts" :move="dragged"
                     @start="drag = true" @end="drag = false"
-                    v-bind="{ animation: 200 }" class="mt-4 mb-40" >
+                    v-bind="{ animation: 200 }" class="mt-4 mb-40 text-center" >
                     <transition-group name="grid-fade">
                         <Post v-for="post in posts" :class="drag ? null : 'item-transition'"
                             :key="post.order" :post="post" />
@@ -65,31 +32,12 @@ export default {
     data () {
         return {
             drag: false,
-            showContextMenu: false
         }
     },
     methods: {
         init () {
             this.$store.dispatch('route/setCurrentRoute', this.$route)
             this.$store.dispatch('post/fetchPosts', this.id)
-        },
-        pasteNewPost () {
-            // may only work with a secure connection
-            navigator.clipboard.readText().then(content => {
-                if (content === '' || content === null) {
-                    return
-                }
-                axios.post('/api/posts', {
-                    content: content,
-                    collection_id: this.currentCollection.id
-                })
-                    .then(response => {
-                        this.$store.dispatch('post/addPost', response.data.data)
-                    })
-                    .catch(error => {
-                        console.log(error)
-                    })
-            })
         },
         dragged (event) {
             axios.patch('/api/posts/' + event.draggedContext.element.id, {
@@ -98,15 +46,6 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
-        },
-        edit () {
-            this.showContextMenu = false
-            this.$router.push({ path: '/c/' + this.currentCollection.id + '/edit' })
-        },
-        del () {
-            this.showContextMenu = false
-            this.$store.dispatch('collection/deleteCollection', this.currentCollection.id)
-            this.$router.push({ path: '/' })
         }
     },
     watch: {
@@ -144,9 +83,6 @@ export default {
         ]),
         ...mapState('collection', [
             'collectionMenu'
-        ]),
-        ...mapState('collection', [
-            'currentCollection'
         ])
     },
     created () {
