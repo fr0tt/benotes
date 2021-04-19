aed8db82ea3a2c8886805f5e76884ad8c7b46ed4
fr0tt <33751346+fr0tt@users.noreply.github.com>
2021-04-20 00:31:59 +0200

    show favicon (how could i forget that ?)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
         <title>Benotes</title>
         <link href="https://rsms.me/inter/inter.css" rel="stylesheet">
         <link href="/css/app.css" type="text/css" rel="stylesheet">
+        <link rel="shortcut icon" type="image/png" href="/logo_144x144.png"/>
         <link rel="manifest" href="./manifest.json">
     </head>
     <body class="h-full">
