72153c9be4de220b50cae27fc17b8268f195fca3
fr0tt <33751346+fr0tt@users.noreply.github.com>
2021-04-29 19:20:12 +0200

    add additional aliases for lumen 7 mail

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,10 @@ $app->withEloquent();
 
 
 $app->configure('mail');
+
+$app->alias('mail.manager', Illuminate\Mail\MailManager::class);
+$app->alias('mail.manager', Illuminate\Contracts\Mail\Factory::class);
+
 $app->alias('mailer', Illuminate\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
