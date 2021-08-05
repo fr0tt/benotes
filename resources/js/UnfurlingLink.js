a1e8efd2367eef87f7d69d8d2b295e92cdccc7fb
fr0tt <33751346+fr0tt@users.noreply.github.com>
2021-08-05 11:51:01 +0200

    add custom tiptap extension

---
+++ resources/js/UnfurlingLink.js
@@ -0,0 +1,135 @@
+import {
+    Node,
+    nodeInputRule,
+    mergeAttributes,
+} from '@tiptap/core'
+import { VueNodeViewRenderer } from '@tiptap/vue-2'
+
+import { Plugin, PluginKey } from 'prosemirror-state'
+import { DecorationSet, Decoration } from 'prosemirror-view'
+
+import Component from './components/UnfurlingLink.vue'
+
+const inputRegex = /^<https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{2,}\b(?:[-a-zA-Z0-9@:%._+~#=?!&/]*)(?:[-a-zA-Z0-9@:%._+~#=?!&/]*)>$/ // /^<.*>$/
+
+export default Node.create({
+
+    name: 'unfurlingLink',
+    group: 'block',
+    atom: true, // "aren’t directly editable and should be treated as a single unit"
+    allowGapCursor: false,
+    draggable: true,
+    //defining: true, // not sure if its important
+    selectable: true,
+    //isolating: true, // not sure if its important
+
+    addAttributes() {
+        return {
+            href: {
+                default: null
+            },
+            'data-title': {
+                default: null
+            }
+        }
+    },
+
+    parseHTML() {
+        return [
+            { tag: 'unfurling-link[data-title]' },
+        ]
+    },
+
+    renderHTML({ HTMLAttributes }) {
+        return ['unfurling-link', mergeAttributes(HTMLAttributes, this.options.HTMLAttributes)/*, 0*/]
+    },
+
+    addNodeView() {
+        return VueNodeViewRenderer(Component)
+    },
+
+    addCommands() {
+        return {
+            setUnfurlingLink: options => ({ tr, dispatch }) => {
+                const { selection } = tr
+                const node = this.type.create(options)
+
+                if (dispatch) {
+                    tr.replaceRangeWith(selection.from, selection.to, node)
+                }
+
+                return true
+            },
+        }
+    },
+
+    addInputRules() {
+        return [
+            nodeInputRule(inputRegex, this.type, match => {
+                const link = match[0].replace(/<|>/g, '')
+                console.log('m', match, match[0], link)
+                return {
+                    href: link
+                }
+            })
+        ]
+    },
+
+    addProseMirrorPlugins() {
+        const mode = 'shallowest' // 'all' | 'deepest' | 'shallowest'
+        return [
+            new Plugin({
+                key: new PluginKey('focus-ufl'),
+                props: {
+                    decorations: ({ doc, selection }) => {
+                        const { isEditable, isFocused } = this.editor
+                        const { anchor } = selection
+                        const decorations = []
+
+                        if (!isEditable || !isFocused) {
+                            return DecorationSet.create(doc, [])
+                        }
+
+                        // Maximum Levels
+                        let maxLevels = 0
+
+                        // Loop through current
+                        let currentLevel = 0
+
+                        doc.descendants((node, pos) => {
+                            if (node.isText) {
+                                return false
+                            }
+
+                            if (node.type.name !== this.name) {
+                                return false
+                            }
+
+                            const isCurrent = anchor >= pos && anchor <= (pos + node.nodeSize - 1)
+
+                            if (!isCurrent) {
+                                return false
+                            }
+
+                            currentLevel += 1
+
+                            const outOfScope = (mode === 'deepest' && maxLevels - currentLevel > 0)
+                                || (mode === 'shallowest' && currentLevel > 1)
+
+                            if (outOfScope) {
+                                return mode === 'deepest'
+                            }
+
+                            decorations.push(Decoration.node(pos, pos + node.nodeSize, {
+                                class: 'has-focus',
+                            }))
+                        })
+
+                        return DecorationSet.create(doc, decorations)
+                    },
+                },
+            }),
+        ]
+    },
+
+})
---
+++ resources/js/components/UnfurlingLink.vue
@@ -0,0 +1,76 @@
+<template>
+    <NodeViewWrapper class="unfurling-link max-w-md block whitespace-normal" >
+        <a class="shadow-md flex my-1"
+            target="_blank" rel="noopener noreferrer nofollow"
+            :href="node.attrs.href"
+            :data-title="node.attrs['data-title']">
+            <img class="w-16 h-16" :src="image">
+            <div class="flex-1 overflow-hidden px-2 bg-white text-gray-900">
+                <span class="block truncate mt-1 text-lg font-semibold">
+                    {{ node.attrs['data-title'] }}
+                </span>
+                <span class="ul-link block mt-1 truncate text-sm text-orange-600 font-medium">
+                    {{ node.attrs.href }}
+                </span>
+            </div>
+        </a>
+    </NodeViewWrapper>
+</template>
+
+<script>
+import { NodeViewWrapper, nodeViewProps } from '@tiptap/vue-2'
+import axios from 'axios'
+
+export default {
+    props: {
+        ...nodeViewProps,
+        updateAttributes: {
+            type: Function,
+            required: true,
+        }
+    },
+    components: {
+        NodeViewWrapper
+    },
+    data () {
+        return {
+            title: '',
+            base_url: '',
+            image: ''
+        }
+    },
+    created () {
+
+        if (this.node.attrs['data-title'] === null || 
+            this.node.attrs['data-title'] === '') {
+            console.log('create if')
+            axios.get('/api/meta', {
+                params: {
+                    url: encodeURI(this.node.attrs.href)
+                }
+            }).then(response => {
+                const data = response.data
+                this.base_url = data.base_url
+                this.title = data.title
+                const domain = this.base_url.replace(/(https|http):\/\//, '')
+                this.image = `https://external-content.duckduckgo.com/ip3/${domain}.ico`
+                this.updateAttributes({
+                    'data-title': data.title,
+                })
+            }).catch(error => {
+                console.log(error)
+            })
+        
+        } else {
+            const domain = new URL(this.node.attrs.href).origin.replace(/https?:\/\//, '')
+            this.image = `https://external-content.duckduckgo.com/ip3/${domain}.ico`
+        }
+        
+    }
+}
+</script>
+<style>
+    .has-focus {
+        box-shadow: 0 1px 3px 0 rgb(255 119 0 / 42%), 0 1px 2px 0 rgb(255 119 0 / 38%);
+    }
+</style>
