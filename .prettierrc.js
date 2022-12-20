a2b6c1155555f0818e325a2f497eab9ae0b8bc08
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-12-21 00:18:30 +0100

    change external config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-    trailingComma: "es5",
+    trailingComma: 'es5',
     tabWidth: 4,
     semi: false,
     singleQuote: true,
     bracketSameLine: true,
-    printWidth: 100
-};
+    printWidth: 100,
+    end_of_line: 'lf',
+}
