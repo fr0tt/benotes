dc6649fb526216f42bbc112cb99fcc1bc7e27d1f
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-02-24 00:43:53 +0100

    use treeview for transfer menu as well

--- resources/js/components/CollectionMenu.vue
+++ resources/js/components/CollectionMenu.vue
@@ -1,49 +1,73 @@
 <template>
     <transition name="fade">
-        <div ref="cmModal" class="absolute top-0 left-0 w-full h-full bg-half-transparent z-50">
-            <div class="w-5/6 max-w-xl m-auto mt-10 border-gray-400 rounded-lg bg-gray-100">
+        <div
+            ref="cmModal"
+            class="fixed top-0 left-0 w-full h-full bg-half-transparent z-50">
+            <div
+                class="w-5/6 max-w-xl m-auto mt-10 border-gray-400 rounded-lg bg-gray-100">
                 <div class="py-8 mx-6">
-                    <h2 class="mb-4 mx-1 text-3xl text-orange-600 font-bold">Collections</h2>
+                    <h2 class="mb-4 mx-1 text-3xl text-orange-600 font-bold">
+                        Collections
+                    </h2>
                     <p class="mb-8 mx-1 gray-600 italic leading-tight">
                         Select a collection you wish to transfer your post to.
                     </p>
-                    <ol v-for="collection in collections" :key="collection.id">
-                        <li
-                            class="py-3 px-4 cursor-pointer hover:text-orange-500 transition-color-0.2"
-                            @click="transfer(collection.id)">
-                            <span class="block font-medium gray-700 open-sans">{{
-                                collection.name
-                            }}</span>
-                        </li>
-                    </ol>
+                    <Treeselect
+                        v-model="selectedCollectionId"
+                        :options="optionsCollections"
+                        :normalizer="
+                            (node) => {
+                                return {
+                                    id: node.id,
+                                    label: node.name,
+                                    children:
+                                        node.nested?.length > 0
+                                            ? node.nested
+                                            : node.children,
+                                }
+                            }
+                        "
+                        placeholder=""
+                        class="block w-80" />
+                    <button class="button block mt-4 ml-auto mr-0" @click="transfer">
+                        Transfer
+                    </button>
                 </div>
             </div>
         </div>
     </transition>
 </template>
 <script>
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 import { mapState } from 'vuex'
 export default {
+    components: {
+        Treeselect,
+    },
+    data() {
+        return {
+            optionsCollections: [],
+            selectedCollectionId: null,
+        }
+    },
     computed: {
-        collections() {
-            const uncategorized = [
-                {
-                    id: null,
-                    name: 'Uncategorized',
-                },
-            ]
-            return uncategorized.concat(this.$store.state.collection.collections)
-        },
+        ...mapState('collection', ['collections']),
         ...mapState('collection', ['collectionMenu']),
     },
     created() {
+        const uncategorized = { name: 'Uncategorized', id: 0, nested: null }
+        this.optionsCollections.push(uncategorized)
+        this.$store.dispatch('collection/fetchCollections', { nested: true }).then(() => {
+            this.optionsCollections = this.optionsCollections.concat(this.collections)
+        })
         document.querySelector('#app').addEventListener('click', this.hide, true)
     },
     methods: {
-        transfer(collectionId) {
+        transfer() {
             // necessary in order to copy the post instead of referencing it
             const post = JSON.parse(JSON.stringify(this.collectionMenu.post))
-            post.collection_id = collectionId
+            post.collection_id = this.selectedCollectionId
             this.$store.dispatch('post/updatePost', { post: post, transfer: true })
             this.$store.dispatch('collection/setCollectionMenu', {
                 isVisible: false,
@@ -56,7 +80,9 @@ export default {
                     isVisible: false,
                     post: null,
                 })
-                document.querySelector('#app').removeEventListener('click', this.hide, true)
+                document
+                    .querySelector('#app')
+                    .removeEventListener('click', this.hide, true)
             }
         },
     },
