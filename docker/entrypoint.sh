89f26375298c6a956d4e80813288479bdebbbc8f
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-05-03 14:55:53 +0200

    change and improve docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-chown -R 1000:www-data storage
+su-exec application php artisan migrate:retry
 
-if [ ! -f /var/www/storage/database.sqlite ] && [ "${DB_CONNECTION}" = "sqlite" ]; then
-    touch /var/www/storage/database.sqlite && chown 1000:1000 /var/www/storage/database.sqlite
-fi
-
-if [ -n "${RUN_MIGRATIONS}" ] && [ "${RUN_MIGRATIONS}" = "true" ]; then
-    php artisan migrate --force
-fi
+supervisord -c /etc/supervisor.d/supervisord.ini
