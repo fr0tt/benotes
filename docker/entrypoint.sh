92a870ddf64d7e59305cebac26ecab83ead0b563
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-05-21 19:47:31 +0200

    undo sqlite change

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -f /var/www/storage/database.sqlite ] && [ "${DB_CONNECTION}" = "sqlite" ]; then
+    touch /var/www/storage/database.sqlite && chown application:www-data /var/www/storage/database.sqlite
+fi
+
 su-exec application php artisan migrate:retry
 
 supervisord -c /etc/supervisor.d/supervisord.ini
