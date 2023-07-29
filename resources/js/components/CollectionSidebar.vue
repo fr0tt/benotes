6815237be2e70414327004b06642bddbd7bfa3f6
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-07-29 12:51:28 +0200

    truncate too long collection names (in sidebar)

--- resources/js/components/CollectionSidebar.vue
+++ resources/js/components/CollectionSidebar.vue
@@ -1,50 +1,33 @@
 <template>
     <li>
-        <div
-            class="collection theme__sidebar__collection"
-            :class="[
-                isActiveLink('/c/' + collection.id)
-                    ? 'router-link-exact-active-parent'
-                    : '',
-            ]"
-            :style="paddingLeft">
-            <router-link
-                class="inline-block w-full"
-                :class="{
-                    inline: isNested(collection),
-                }"
-                :to="'/c/' + collection.id">
-                <svg-vue
-                    v-if="collectionIconIsInline(collection.icon_id)"
-                    :icon="'glyphs/' + collection.icon_id"
+        <div class="collection theme__sidebar__collection" :class="[
+            isActiveLink('/c/' + collection.id)
+                ? 'router-link-exact-active-parent'
+                : '',
+        ]" :style="paddingLeft">
+            <router-link class="truncate" :class="{
+                inline: isNested(collection),
+            }" :to="'/c/' + collection.id">
+                <svg-vue v-if="collectionIconIsInline(collection.icon_id)" :icon="'glyphs/' + collection.icon_id"
                     class="w-6 h-6 glyphs" />
                 <svg v-else-if="collection.icon_id" class="w-6 h-6 glyphs">
                     <use :xlink:href="'/glyphs.svg#' + collection.icon_id" />
                 </svg>
-                <svg-vue
-                    v-else
-                    icon="remix/folder-fill"
-                    class="no-glyph w-4 fill-current align-text-bottom mr-2" />
+                <svg-vue v-else icon="remix/folder-fill" class="no-glyph w-4 fill-current align-text-bottom mr-2" />
                 <span class="theme__sidebar__label align-middle text-gray-700">{{
                     collection.name
                 }}</span>
             </router-link>
 
-            <button v-if="isNested(collection)" @click="show = !show">
-                <svg-vue
-                    class="icon transform transition-transform duration-300 text-gray-600"
-                    :class="{ 'rotate-180': show }"
-                    icon="material/arrow_drop_down" />
+            <button v-if="isNested(collection)" @click="show = !show" class="flex-auto text-left pl-1">
+                <svg-vue class="icon transform transition-transform duration-300 text-gray-600"
+                    :class="{ 'rotate-180': show }" icon="material/arrow_drop_down" />
             </button>
         </div>
         <transition name="fade">
             <ol v-if="show">
-                <CollectionSidebar
-                    v-for="nestedCollection in collection.nested"
-                    :key="nestedCollection.id"
-                    :collection="nestedCollection"
-                    :level="level + 1"
-                    class="nested -mb-1" />
+                <CollectionSidebar v-for="nestedCollection in collection.nested" :key="nestedCollection.id"
+                    :collection="nestedCollection" :level="level + 1" class="nested -mb-1" />
             </ol>
         </transition>
     </li>
--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,120 +1,67 @@
 <template>
     <transition name="sidebar-slide">
-        <div
-            v-if="showSidebar"
-            class="sidebar w-full md:w-48 lg:w-64 xl:w-1/6 theme__sidebar">
+        <div v-if="showSidebar" class="sidebar w-full md:w-48 lg:w-64 xl:w-1/6 theme__sidebar">
             <div class="list pt-2">
                 <ol class="mb-12">
                     <li>
-                        <router-link
-                            to="/search"
-                            class="collection theme__sidebar__collection">
-                            <svg-vue
-                                class="w-4 fill-current mr-2"
-                                icon="remix/search-line" />
-                            <span class="align-middle text-gray-700 theme__sidebar__label"
-                                >Search</span
-                            >
+                        <router-link to="/search" class="collection theme__sidebar__collection">
+                            <svg-vue class="w-4 fill-current mr-2" icon="remix/search-line" />
+                            <span class="align-middle text-gray-700 theme__sidebar__label">Search</span>
                         </router-link>
                     </li>
                     <li>
-                        <router-link
-                            to="/tags"
-                            class="collection theme__sidebar__collection">
-                            <svg-vue
-                                class="w-5 fill-current mr-1"
-                                icon="material/label" />
-                            <span class="align-middle text-gray-700 theme__sidebar__label"
-                                >Tags</span
-                            >
+                        <router-link to="/tags" class="collection theme__sidebar__collection">
+                            <svg-vue class="w-5 fill-current mr-1" icon="material/label" />
+                            <span class="align-middle text-gray-700 theme__sidebar__label">Tags</span>
                         </router-link>
                     </li>
                     <li>
-                        <router-link
-                            to="/restore"
-                            class="collection theme__sidebar__collection"
-                            :class="{
-                                'router-link-exact-active': isActiveLink('/restore'),
-                            }">
-                            <svg-vue
-                                icon="zondicons/trash"
-                                class="w-4 fill-current align-text-bottom mr-2" />
-                            <span class="align-middle text-gray-700 theme__sidebar__label"
-                                >Recycle Bin</span
-                            >
+                        <router-link to="/restore" class="collection theme__sidebar__collection" :class="{
+                            'router-link-exact-active': isActiveLink('/restore'),
+                        }">
+                            <svg-vue icon="zondicons/trash" class="w-4 fill-current align-text-bottom mr-2" />
+                            <span class="align-middle text-gray-700 theme__sidebar__label">Recycle Bin</span>
                         </router-link>
                     </li>
                     <li>
-                        <router-link
-                            to="/users"
-                            class="collection theme__sidebar__collection">
-                            <svg-vue
-                                class="w-4 fill-current mr-2"
-                                icon="remix/group-fill" />
-                            <span class="align-middle text-gray-700 theme__sidebar__label"
-                                >Users</span
-                            >
+                        <router-link to="/users" class="collection theme__sidebar__collection">
+                            <svg-vue class="w-4 fill-current mr-2" icon="remix/group-fill" />
+                            <span class="align-middle text-gray-700 theme__sidebar__label">Users</span>
                         </router-link>
                     </li>
                     <li>
-                        <router-link
-                            :to="'/users/' + authUser.id"
-                            class="collection theme__sidebar__collection">
-                            <svg-vue
-                                class="w-4 fill-current mr-2"
-                                icon="remix/user-settings-fill" />
-                            <span class="align-middle text-gray-700 theme__sidebar__label"
-                                >My Account</span
-                            >
+                        <router-link :to="'/users/' + authUser.id" class="collection theme__sidebar__collection">
+                            <svg-vue class="w-4 fill-current mr-2" icon="remix/user-settings-fill" />
+                            <span class="align-middle text-gray-700 theme__sidebar__label">My Account</span>
                         </router-link>
                     </li>
                     <li class="collection theme__sidebar__collection">
                         <a @click="logout()">
-                            <svg-vue
-                                class="w-4 fill-current mr-2"
-                                icon="remix/logout-circle-line" />
-                            <span class="align-middle text-gray-700 theme__sidebar__label"
-                                >Logout</span
-                            >
+                            <svg-vue class="w-4 fill-current mr-2" icon="remix/logout-circle-line" />
+                            <span class="align-middle text-gray-700 theme__sidebar__label">Logout</span>
                         </a>
                     </li>
                 </ol>
-                <router-link
-                    to="/"
-                    class="collection mb-4 theme__sidebar__collection"
+                <router-link to="/" class="collection mb-4 theme__sidebar__collection"
                     :class="{ 'router-link-exact-active': isActiveLink('/') }">
-                    <svg-vue
-                        icon="remix/folder-unknow-fill"
-                        class="w-4 fill-current align-text-bottom mr-2" />
-                    <span class="align-middle text-gray-700 theme__sidebar__label"
-                        >Uncategorized</span
-                    >
+                    <svg-vue icon="remix/folder-unknow-fill" class="w-4 fill-current align-text-bottom mr-2" />
+                    <span class="align-middle text-gray-700 theme__sidebar__label">Uncategorized</span>
                 </router-link>
-                <span
-                    class="mb-2 md:px-8 px-4 block text-xs text-gray-700 font-medium uppercase theme__sidebar__subhead">
+                <span class="mb-2 md:px-8 px-4 block text-xs text-gray-700 font-medium uppercase theme__sidebar__subhead">
                     Collections
                 </span>
 
                 <ol>
-                    <CollectionSidebar
-                        v-for="collection in collections"
-                        :key="collection.id"
-                        :collection="collection" />
+                    <CollectionSidebar v-for="collection in collections" :key="collection.id" :collection="collection" />
                 </ol>
             </div>
-            <router-link
-                to="/c/create"
-                class="block md:mx-8 mx-4 mt-4 text-orange-600 font-medium">
-                <svg-vue
-                    class="w-4 mr-2 fill-current align-text-bottom"
-                    icon="remix/folder-add-fill" />
+            <router-link to="/c/create" class="block md:mx-8 mx-4 mt-4 text-orange-600 font-medium">
+                <svg-vue class="w-4 mr-2 fill-current align-text-bottom" icon="remix/folder-add-fill" />
                 <span class="align-middle">Create a new collection</span>
             </router-link>
             <div class="w-full px-4 md:px-6 mb-0 mt-auto pt-12">
                 <svg-vue class="w-6 align-text-bottom" icon="logo_64x64" />
-                <span class="flex-1 ml-1 text-orange-600 text-xl font-medium"
-                    >Benotes</span
-                >
+                <span class="flex-1 ml-1 text-orange-600 text-xl font-medium">Benotes</span>
             </div>
         </div>
     </transition>
@@ -178,92 +125,113 @@ export default {
 .min-w-48 {
     min-width: 12rem;
 }
+
 .py-1\.5 {
     padding-top: 0.375rem;
     padding-bottom: 0.375rem;
 }
+
 .bottom-4 {
     bottom: 1rem;
 }
+
 .sidebar-slide-enter-active {
     transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s, opacity 0.5s;
     -webkit-transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s, opacity 0.5s;
     -moz-transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s, opacity 0.5s;
 }
+
 .sidebar-slide-leave-active {
     transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s;
     -webkit-transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s;
     -moz-transition: width cubic-bezier(0.4, 0, 0.2, 1) 0.35s;
 }
+
 .sidebar-slide-enter,
 .sidebar-slide-leave-to {
     width: 0;
     opacity: 0;
 }
+
 .sidebar {
     @apply h-full fixed flex flex-col pb-6 pt-20 overflow-y-auto z-40 bg-white;
     font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
         Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     box-shadow: 2px 3px 3px 0 rgba(0, 0, 0, 0.1);
+
     svg {
         display: inline-block;
     }
+
     .menu {
         margin-top: -0.9rem;
+
         .menuItem {
             @apply block py-1.5 px-4 font-medium text-gray-700;
             transition: 0.2s background-color;
         }
+
         .menuItem:hover {
             @apply bg-gray-300;
         }
     }
+
     .menu-icon {
         width: 1.75rem;
     }
+
     .list {
         .collection {
-            @apply inline-block w-full py-1;
+            @apply flex w-full py-1;
             @apply font-medium text-gray-500;
             @apply pl-4;
             border-left: 3px solid;
             border-color: transparent;
+
             a {
                 @apply cursor-pointer;
             }
         }
+
         @screen sm {
             .collection {
                 @apply pl-8;
             }
         }
+
         .nested .collection {
             @apply pl-4;
         }
+
         .glyphs {
             margin-left: -0.25rem;
             margin-right: 0.25rem;
         }
     }
+
     // outside of .list in order to be able to be overriden
     .router-link-exact-active-parent,
     .collection.router-link-exact-active {
         @apply bg-orange-200 text-gray-700 border-orange-600 font-semibold;
     }
 }
+
 .sidebar::-webkit-scrollbar {
     width: 3px;
     height: 3px;
     background-color: #f5f5f5;
 }
+
 .sidebar::-webkit-scrollbar-track {
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     background-color: rgba(255, 255, 255, 0.8);
 }
+
 .sidebar::-webkit-scrollbar-thumb {
     background-color: #86827d;
     border-radius: 8px;
 }
+
 .sidebar .collection .fade-enter-active,
 .sidebar .collection.fade-leave-active {
     transition: opacity 0.1s;
