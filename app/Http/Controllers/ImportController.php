894b5b65e3549f61eb0ae46a2b2d687d9f015c2e
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-08-08 12:44:37 +0200

    add bookmark importer

---
+++ app/Http/Controllers/ImportController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Collection;
+use App\Helpers\NetscapeBookmarkDecoder;
+use App\Services\PostService;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ImportController extends Controller
+{
+
+    private $service;
+
+    public function __construct()
+    {
+        $this->service = new PostService();
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'file' => 'file|mimetypes:text/html|required',
+        ]);
+
+        $collection = Collection::firstOrCreate([
+            'name'    => 'Imported Bookmarks',
+            'user_id' => Auth()->user()->id
+        ]);
+
+        $parser = new NetscapeBookmarkDecoder(Auth()->user()->id);
+        $parser->parseFile($request->file('file'), $collection->id);
+
+        return response()->json('', Response::HTTP_CREATED);
+    }
+
+}
